3dfdac62a756955adee35264a6b303c2261d09a0
Raunak shahu <raunakshahu.cg@gmail.com>
2025-08-28 16:05:45 +0530

    factorial

---
+++ factoril.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, fact = 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i)
+    {
+        fact *= i; // fact = fact*i;
+    }
+    printf("Factorial of %d = %d\n", n, fact);
+    return 0;
+}
